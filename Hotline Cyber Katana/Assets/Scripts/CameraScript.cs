5f4047ede85fa0ecf0e57af2e50a43c81cec9e0f
yanchernyavsky <chernyavsky1299@gmail.com>
2019-10-30 13:46:10 +0300

    Revert "Revert "init""
    This reverts commit c7a5b4e7d37d426e66d30286b52493d739d53fc8.

---
+++ Hotline Cyber Katana/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform trackedObject, targetCamera;
+    public Vector3 offset;
+    Transform currentTrackedObject;
+ 
+    void Start()
+    {
+        currentTrackedObject = trackedObject;
+    }
+    void FixedUpdate()
+    {
+        targetCamera.position = Vector3.Lerp(targetCamera.position, currentTrackedObject.position, 0.05f) + offset;
+    }
+}
