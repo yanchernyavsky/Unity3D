1ad29b9a59406e32d8ee50808f764399d11be53e
yanchernyavsky <chernyavsky1299@gmail.com>
2019-10-30 13:43:51 +0300

    init

---
+++ Hotline Cyber Katana/Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    GameObject Bullet;
+    bool allowfire = true;
+    public float ScaleRateOFFire;
+
+    private void Update()
+    {
+        if (true)
+        {
+
+        }
+    }
+    public void Shoot()
+    {
+        if(allowfire)
+        {
+
+        allowfire = false;
+        Bullet = Instantiate(BulletPrefab, transform.position, transform.rotation);
+        gameObject.GetComponent<AudioSource>().Play();
+        StartCoroutine(RateOfFire());
+
+        }
+    }
+    IEnumerator RateOfFire()
+    {
+        yield return new WaitForSeconds(ScaleRateOFFire);
+        allowfire = true;
+    }
+}
